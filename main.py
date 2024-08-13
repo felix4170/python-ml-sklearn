9a712cfbc3d3a0226f057115d24c22c7bb002f6b
felix4170 <felix44291@gmail.com>
2024-08-12 23:34:36 -0700

    fix:project finished

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-import pandas as pd
+# load the iris dataset as an example
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+# store the feature matrix (X) and response vector (y)
+X = iris.data
+y = iris.target
+
+# splitting X and y into training and testing sets
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
 
-# Load dataset
-data = pd.read_csv('customer_churn.csv')
+# training the model on training set
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=3)
 
+knn.fit(X_train, y_train)
 
-# Encode categorical variables
-Labelencoder = LabelEncoder()
-data['Gender'] = Labelencoder.fit_transfrom(data['Gender'])
-data['Geography'] = Labelencoder.fit_transform(data['Geography'])
+# making predictions on the testing set
+y_pred = knn.predict(X_test)
 
+# comparing actual response values (y_test) with predicted response values (y_pred)
+from sklearn import metrics
+print("KNN model accuracy", metrics.accuracy_score(y_test, y_pred))
 
-# Feature selection
-X = data.drop('Exited', axis=1)
-y = data['Exited']
-
-# Train/Test split
+# making prediction for out of sample data
+sample = [[3, 5, 4, 2], [2, 3, 5, 4]]
+preds = knn.predict(sample)
+pred_species = [iris.target_names[p] for p in preds]
+print("Predictions", pred_species)
